a18d530b7ccdee0e4dea637198ac0215b015db03
MarkelOnaindia <markel.onaindia@ikasle.egibide.org>
2023-10-20 13:45:57 +0200

    ejercicio4

---
+++ 01-Introduccion/ejercicio04.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejercicio02</title>
+</head>
+<body>
+    <?php echo
+        "<h1>Introduccion</h1>
+         <h3>Ejercicio2</h3>";
+
+         $nombre =  $_GET["nombre"];
+         $edad =  $_GET["edad"];
+         echo "Mi amigo $nombre tiene $edad años"
+    ?>
+
+
+</body>
+</html>
