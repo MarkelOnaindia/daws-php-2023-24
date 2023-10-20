fe55a8dab29f2c96ea5649b872ee77fb302eec8b
MarkelOnaindia <markel.onaindia@ikasle.egibide.org>
2023-10-20 13:31:44 +0200

    cambiar nombre ejercicio

---
+++ 01-Introduccion/ejercicio01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejercicio01</title>
+</head>
+<body>
+    <?php echo
+        "<h1>Introduccion</h1>
+         <h3>Ejercicio1</h3>";
+
+         $nombre = "Markel";
+         $edad = "20";
+    ?>
+
+    <?= $nombre, $edad?>
+
+</body>
+</html>
