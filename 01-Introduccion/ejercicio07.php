1b62b6da710d718066d0b8b293fa1419004d4399
MarkelOnaindia <markel.onaindia@ikasle.egibide.org>
2023-10-20 14:08:03 +0200

    ejercicio8

---
+++ 01-Introduccion/ejercicio07.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejercicio06</title>
+</head>
+<body>
+    <?php echo
+        "<h1>Introduccion</h1>
+         <h3>Ejercicio6</h3>";
+
+         $a =  $_GET["a"];
+         $b =  $_GET["b"];
+
+         function multiplicar($a,$b){
+            return $a * $b;
+         }
+
+         echo multiplicar($a,$b);
+    ?>
+
+
+</body>
+</html>
