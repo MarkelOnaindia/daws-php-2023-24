44224735aa33db0938be09ecefca57817b1af9ed
MarkelOnaindia <markel.onaindia@ikasle.egibide.org>
2023-10-20 13:43:21 +0200

    ejercicio3

---
+++ 01-Introduccion/ejercicio03.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejercicio03</title>
+</head>
+<body>
+    <?php echo
+        "<h1>Introduccion</h1>
+         <h3>Ejercicio3</h3>";
+
+         $nombre = $_GET["nombre"];
+         echo "Bienvenido $nombre."
+    ?>
+
+</body>
+</html>
