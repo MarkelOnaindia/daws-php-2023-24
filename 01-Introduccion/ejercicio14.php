4f87b3ab5a557cc85a63e4cd5b7de5c09b0ffa83
MarkelOnaindia <markel.onaindia@ikasle.egibide.org>
2023-10-23 13:12:15 +0200

    ejercicio 14

---
+++ 01-Introduccion/ejercicio14.php
@@ -0,0 +1,11 @@
+<?php
+
+$traducciones = array(
+    "perro" => "dog",
+    "gato" => "cat",
+    "coche" => "car",
+    "casa" => "house",
+    "otorrinolaringólogo" => "otorhinolaryngologist"
+);
+
+require 'ejercicio14.view.php';
---
+++ 01-Introduccion/ejercicio14.view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejercicio14</title>
+</head>
+<body>
+    <h1>Introducción</h1>
+    <h3>Ejercicio 14</h3>
+
+    <?php
+    echo "La traducción de otorrinolaringólogo en inglés es {$traducciones['otorrinolaringólogo']}<br>";
+    echo "La traducción de perro en inglés es {$traducciones['perro']}<br>";
+    echo "La traducción de gato en inglés es {$traducciones['gato']}<br>";
+    echo "La traducción de coche en inglés es {$traducciones['casa']}<br>";
+
+   ?>
+</body>
+</html>
