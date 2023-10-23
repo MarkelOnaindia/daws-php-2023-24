1877f8428896824ceb4ded8615f97bddf93a00cc
MarkelOnaindia <markel.onaindia@ikasle.egibide.org>
2023-10-23 08:41:59 +0200

    correccion ejercicio 7

--- 01-Introduccion/ejercicio07.php
+++ 01-Introduccion/ejercicio07.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ejercicio06</title>
+    <title>ejercicio07</title>
 </head>
 <body>
     <?php echo
         "<h1>Introduccion</h1>
-         <h3>Ejercicio6</h3>";
+         <h3>Ejercicio7</h3>";
 
          $a =  $_GET["a"];
          $b =  $_GET["b"];
