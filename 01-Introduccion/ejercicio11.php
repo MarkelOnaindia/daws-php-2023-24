df75f05432304427e9b6007fc6b49f056ab3dfce
MarkelOnaindia <markel.onaindia@ikasle.egibide.org>
2023-10-23 12:24:53 +0200

    ejercicio 11

---
+++ 01-Introduccion/ejercicio11.php
@@ -0,0 +1,11 @@
+<?php
+function concatenarCadenas($cadena1, $cadena2 = " Amigos!") {
+    $resultado = $cadena1 . $cadena2;
+    return $resultado;
+}
+
+$texto1 = "Hola, ";
+$concatenacion = concatenarCadenas($texto1);
+
+require 'ejercicio11.view.php';
+?>
---
+++ 01-Introduccion/ejercicio11.view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejercicio11</title>
+</head>
+<body>
+    <?php        
+    echo "<h1>Introducción</h1>";
+    echo "<h3>Ejercicio 11</h3>";
+    echo "<p>Concatenación de cadenas: " . $concatenacion . "</p>";
+    ?>
+</body>
+</html>
