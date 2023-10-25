4e7f2a0b9efc60800b1fdd7613ad3f180bbb7b4b
MarkelOnaindia <markel.onaindia@ikasle.egibide.org>
2023-10-25 12:38:53 +0200

    ejercicio 24

---
+++ 01-Introduccion/ejercicio24.php
@@ -0,0 +1,8 @@
+<?php
+$agenda = [
+    ["nombre" => "Juan", "apellidos" => "Pérez", "telefono" => "123456789", "correo" => "juan@example.com"],
+    ["nombre" => "María", "apellidos" => "Gómez", "telefono" => "987654321", "correo" => "maria@example.com"],
+    ["nombre" => "Carlos", "apellidos" => "López", "telefono" => "111222333", "correo" => "carlos@example.com"]
+];
+
+require 'ejercicio24.view.php';
---
+++ 01-Introduccion/ejercicio24.view.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 24</title>
+</head>
+<body>
+    <h1>Introducción</h1>
+    <h3>Ejercicio 22</h3>
+    <h1>Agenda de Contactos</h1>
+    <table border="1">
+        <tr>
+            <th>Nombre</th>
+            <th>Apellidos</th>
+            <th>Teléfono</th>
+            <th>Correo Electrónico</th>
+        </tr> 
+        <?php
+        for ($i = 0; $i < count($agenda); $i++) {
+            echo "<tr>";
+                echo "<td>{$agenda[$i]['nombre']}</td>";
+                echo "<td>{$agenda[$i]['apellidos']}</td>";
+                echo "<td>{$agenda[$i]['telefono']}</td>";
+                echo "<td>{$agenda[$i]['correo']}</td>";
+            echo "</tr>";
+        }
+        ?>
+    </table>
+</body>
+</html>
