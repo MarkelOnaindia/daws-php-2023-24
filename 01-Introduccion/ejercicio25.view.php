6923551b2a14e6677b4899230a65193f576531f0
MarkelOnaindia <markel.onaindia@ikasle.egibide.org>
2023-10-25 12:41:17 +0200

    ejercicio 25

---
+++ 01-Introduccion/ejercicio25.view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 25</title>
+</head>
+<body>
+    <h1>Introducción</h1>
+    <h3>Ejercicio 22</h3>
+    <h1>Lista de Estudiantes</h1>
+    <ul>
+        <?php
+        $indice = 0;
+        while ($indice < count($estudiantes)) {
+            echo "<li>{$estudiantes[$indice]}</li>";
+            $indice++;
+        }
+        ?>
+    </ul>
+</body>
+</html>
