0cd72af4395039492d599d62834f33a3ef76801b
MarkelOnaindia <126677198+MarkelOnaindia@users.noreply.github.com>
2023-10-30 09:51:15 +0100

    Delete 03-SesionesYCookies/ejercicio01.php

--- 03-SesionesYCookies/ejercicio01.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $usuario = $_POST["usuario"];
-    setcookie("usuario", $usuario);
-    header("Location: ejercicio01.php");
-    exit();
-}
-require 'ejercicio01.view.php';
