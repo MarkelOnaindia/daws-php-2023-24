233b36243e3159415b30a84dab9ccdc8e8052092
MarkelOnaindia <markel.onaindia@ikasle.egibide.org>
2023-11-06 10:40:43 +0100

    BD practica 4

--- 04-AccesoADatos/practica4BD.sql
+++ 04-AccesoADatos/practica4BD.sql
@@ -1,13 +1,61 @@
--- Crea la tabla alumno con los campos id (auto-incremental), nombre, apellidos, email y edad
-CREATE TABLE alumno (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL,
-    apellidos VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    edad INT
-);
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         8.0.30 - MySQL Community Server - GPL
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             12.1.0.6537
+-- --------------------------------------------------------
 
--- Inserta algunos registros manualmente
-INSERT INTO alumno (nombre, apellidos, email, edad) VALUES ('Juan', 'Pérez', 'juan@example.com', 25);
-INSERT INTO alumno (nombre, apellidos, email, edad) VALUES ('María', 'Gómez', 'maria@example.com', 22);
-INSERT INTO alumno (nombre, apellidos, email, edad) VALUES ('Carlos', 'López', 'carlos@example.com', 24);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Volcando estructura de base de datos para practica4
+CREATE DATABASE IF NOT EXISTS `practica4` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `practica4`;
+
+-- Volcando estructura para tabla practica4.alumno
+CREATE TABLE IF NOT EXISTS `alumno` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `apellidos` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `edad` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla practica4.empleados
+CREATE TABLE IF NOT EXISTS `empleados` (
+  `dni` varchar(9) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `nombre` varchar(15) DEFAULT NULL,
+  `apellidos` varchar(15) DEFAULT NULL,
+  `edad` int DEFAULT NULL,
+  `sexo` varchar(10) DEFAULT NULL,
+  `fecha_de_nacimiento` date DEFAULT NULL,
+  `curriculum` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`dni`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla practica4.lista_compra
+CREATE TABLE IF NOT EXISTS `lista_compra` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `elemento` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
