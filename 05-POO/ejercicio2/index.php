1d34eaddc303eca6267ab9bf70e9c2c7448556d0
MarkelOnaindia <markel.onaindia@ikasle.egibide.org>
2024-01-08 12:12:34 +0100

    cor

--- 05-POO/ejercicio2/index.php
+++ 05-POO/ejercicio2/index.php
@@ -3,7 +3,7 @@ include 'models/publicacion.php';
 
 use ejercicio2\models\Publicacion;
 
-$p1 = new Publicacion("Markel", "1999", "WIT", "Pepe botella", "Menudo botellon jejeje");
+$p1 = new Publicacion("Markel", "1999", "WIT", "Pepe botella", "Menudo jejeje");
 $p1->leer();
 echo "<br>";
 $p1->escribir(" PepePepePepe");
